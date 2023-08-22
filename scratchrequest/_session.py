a1707d7dd76c469f68dc87ea7343cceb45938138
programORdie2 <125064160+programORdie2@users.noreply.github.com>
2023-08-22 21:02:31 +0200

    Add files via upload

--- scratchrequest/_session.py
+++ scratchrequest/_session.py
@@ -5,37 +5,18 @@ from . import _project
 from . import _settings
 import requests, json, re
 
-headers = {
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36",
-    "x-csrftoken": "a",
-    "x-requested-with": "XMLHttpRequest",
-    "referer": "https://scratch.mit.edu",
-}
+headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36", "x-csrftoken": "a", "x-requested-with": "XMLHttpRequest", "referer": "https://scratch.mit.edu"}
 
 class Session:
-    def __init__(self, username : str=None, *, session_id : str):
+    def __init__(self, username : str, session_id : str):
         self.session_id = session_id
         self.username = username
         self.headers = headers
         self._login()
 
     def _login(self):
-        '''
-        Don't use this
-        '''
-        self.cookies = {
-            "scratchcsrftoken" : "a",
-            "scratchlanguage" : "en",
-            "scratchpolicyseen": "true",
-            "scratchsessionsid" : self.session_id,
-            "accept": "application/json",
-            "Content-Type": "application/json",
-        }
-        account = requests.post("https://scratch.mit.edu/session", headers=self.headers, cookies={
-            "scratchsessionsid": self.session_id,
-            "scratchcsrftoken": "a",
-            "scratchlanguage": "en",
-        }).json()
+        self.cookies = {"scratchcsrftoken" : "a", "scratchlanguage" : "en", "scratchpolicyseen": "true", "scratchsessionsid" : self.session_id, "accept": "application/json", "Content-Type": "application/json"}
+        account = requests.post("https://scratch.mit.edu/session", headers=self.headers, cookies={"scratchsessionsid": self.session_id, "scratchcsrftoken": "a", "scratchlanguage": "en"}).json()
         try:
             self.xtoken = account["user"]["token"]
             self.headers["X-Token"] = self.xtoken
@@ -56,43 +37,16 @@ class Session:
         
     @classmethod
     def login(cls, username : str, password : str):
-        '''
-        Login from your username and password.
-        '''
         try:
-            return cls(
-                username, session_id=str(re.search('"(.*)"', requests.post(
-                    "https://scratch.mit.edu/login/",
-                    data=json.dumps({
-                        "username": username,
-                        "password": password
-                    }),
-                    headers=headers,
-                    cookies={
-                        "scratchcsrftoken": "a",
-                        "scratchlanguage": "en"
-                    }
-                ).headers["Set-Cookie"]).group())
-            )
+            return cls(username, session_id=str(re.search('"(.*)"', requests.post("https://scratch.mit.edu/login/", data=json.dumps({"username": username, "password": password}), headers=headers, cookies={"scratchcsrftoken": "a", "scratchlanguage": "en"}).headers["Set-Cookie"]).group()))
         except AttributeError:
             raise LoginError("Your login data was wrong. Check if you spelled your credits correctly, or if you are in replit, see the docs for more information.")
-        except Exception as e:
-            raise Exception("An error occurred while trying to log in.") from e
         
-    def CloudConnection(self, project_id : int, **kwargs) -> _cloud.CreateCloudConnection:
-        '''
-        Create a cloud connection to a project.
-        '''
-        return _cloud.CreateCloudConnection(project_id=project_id, session=self, **kwargs)
+    def CloudConnection(self, project_id : int) -> _cloud.CreateCloudConnection:
+        return _cloud.CreateCloudConnection(project_id=project_id, session=self)
 
     def ConnectProject(self, id : int) -> _project.CreateConnectProject:
-        '''
-        Create a connection to a project.
-        '''
         return _project.CreateConnectProject(id=id, session=self)
 
     def ManageSettings(self) -> _settings.ManageSettings:
-        '''
-        Makes it possible to edit your settings.
-        '''
         return _settings.ManageSettings(session=self)
