8951f657e705575ff2ea7c5b3f2aa5d4e4576f25
programORdie2 <125064160+programORdie2@users.noreply.github.com>
2023-08-25 21:39:49 +0200

    Add files via upload

--- scratchrequest/__init__.py
+++ scratchrequest/__init__.py
@@ -2,8 +2,5 @@ from ._session import *
 from ._twcloud import *
 from ._project import *
 
-def Login(username : str, password : str):
-  return Session.login(username=username, password=password)
-
 def get_project(id: int):
   return CreateConnectProject(id=id)
--- scratchrequest/_session.py
+++ scratchrequest/_session.py
@@ -7,7 +7,7 @@ import requests, json, re
 headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36", "x-csrftoken": "a", "x-requested-with": "XMLHttpRequest", "referer": "https://scratch.mit.edu"}
 
 class Session:
-    def __init__(self, username : str, session_id : str):
+    def __init__(self, username: str, session_id: str):
         self.session_id = session_id
         self.username = username
         self.headers = headers
@@ -34,18 +34,24 @@ class Session:
         request = requests.get("https://scratch.mit.edu/csrf_token/", headers=headers)
         self.csrf_token = re.search("scratchcsrftoken=(.*?);", request.headers["Set-Cookie"]).group(1)
         
-    @classmethod
-    def login(cls, username : str, password : str):
+    def _login_pass(username: str, password: str):
         try:
-            return cls(username, session_id=str(re.search('"(.*)"', requests.post("https://scratch.mit.edu/login/", data=json.dumps({"username": username, "password": password}), headers=headers, cookies={"scratchcsrftoken": "a", "scratchlanguage": "en"}).headers["Set-Cookie"]).group()))
+            response = requests.post("https://scratch.mit.edu/login/", data=json.dumps({"username": username, "password": password}), headers=headers, cookies={"scratchcsrftoken": "a", "scratchlanguage": "en"})
+            response = response.headers["Set-Cookie"]).group()
+            self.username = username
+            self.session_id = re.search('"(.*)"', response)
+            return _login()
         except AttributeError:
             raise LoginError("Your login data was wrong. Check if you spelled your credits correctly, or if you are in replit, see the docs for more information.")
         
-    def CloudConnection(self, project_id : int) -> _cloud.CreateCloudConnection:
+    def CloudConnection(self, project_id: int):
         return _cloud.CreateCloudConnection(project_id=project_id, session=self)
 
-    def ConnectProject(self, id : int) -> _project.CreateConnectProject:
+    def ConnectProject(self, id: int):
         return _project.CreateConnectProject(id=id, session=self)
 
-    def ManageSettings(self) -> _settings.ManageSettings:
+    def ManageSettings(self):
         return _settings.ManageSettings(session=self)
+
+def Login(username: str, password: str):
+    return Session._login_pass(username=username, password=username)
